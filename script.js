cfc9c73b0f41c261198e1c292f36fa1c7768c693
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2019-05-25 07:26:38 +0100

    neatened it up

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const scrollReveal = function(){
     const progressBars = document.querySelectorAll('.progress-bar');
-    console.log(progressBars);
-    // progressBars.forEach(section => section.classList.add('hidden'));
 
     const isInViewport = (elem) => {
-        return elem.top <= window.innerHeight;
+        return elem.getBoundingClientRect().top <= window.innerHeight;
     }
 
     window.addEventListener("scroll", () =>{
         progressBars.forEach(bar => {
-            if (isInViewport(bar.getBoundingClientRect())){
+            if (isInViewport(bar)){
                 bar.classList.add('progress-bar--active');
             }
         })
